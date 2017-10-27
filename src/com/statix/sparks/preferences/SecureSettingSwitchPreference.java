1fa16a5600a7d810f9dce73a09e94efeda1490de
Alex Cruz <du.alexcruz@gmail.com>
2017-10-26 23:03:07 -0400

    Update imports for preferences
    Change-Id: I3435a9214b12615f5198ac304943f1860d7eeb77

--- src/com/statix/sparks/preferences/SecureSettingSwitchPreference.java
+++ src/com/statix/sparks/preferences/SecureSettingSwitchPreference.java
@@ -17,7 +17,7 @@
 package com.statix.sparks.preferences;
 
 import android.content.Context;
-import android.preference.SwitchPreference;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 import android.util.AttributeSet;
 
