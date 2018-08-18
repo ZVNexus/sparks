3dbb819970e79e726eaefbcabb2860cc2b8bd4c1
Adin Kwok <adin.kwok@carbonrom.org>
2018-08-18 15:25:17 -0700

    Sparks: Add battery customizations [2/2]
    Adin's edit: late night code grind, I want to sleep...
    
    Squash of the following:
    From: tsubus <tsubus@ilwt.org>
    Date: Wed, 5 Oct 2016 02:52:00 -0700
    Subject: [PATCH] Battery Customizations
    Change-Id: I5cb20a9f75e573c306189f5781c9274784217a88
    
    From: ezio84 <brabus84@gmail.com>
    Date: Mon, 12 Feb 2018 22:44:27 +0100
    Subject: [PATCH] Move tuner percentage settings to battery settings [2/2]
    Also fix up arrays and what not
    Change-Id: I4ba78a067af8caadff46d8f16b959efcb5570fb1
    
    From: xyyx <xyyx@mail.ru>
    Date: Thu, 15 Feb 2018 13:20:21 +0800
    Subject: [PATCH] BatteryPercent: Allow disabling percent inside circle
    Change-Id: I1fb29757dfe5b547935536df81ab7dd0376da634
    
    Preserving first patch's Change-Id
    
    Change-Id: I5cb20a9f75e573c306189f5781c9274784217a88

--- src/com/statix/sparks/fragments/statusbar/Statusbar.java
+++ src/com/statix/sparks/fragments/statusbar/Statusbar.java
@@ -17,21 +17,69 @@
 package com.statix.sparks.fragments.statusbar;
 
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.settings.R;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settingslib.graph.BatteryMeterDrawableBase;
 import com.statix.sparks.preferences.CustomSettingsPreferenceFragment;
 
-public class Statusbar extends CustomSettingsPreferenceFragment {
+
+public class Statusbar extends CustomSettingsPreferenceFragment implements
+	    OnPreferenceChangeListener {
     private static final String TAG = "Status bar";
+    private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+    private static final String BATTERY_PERCENT = "show_battery_percent";
+
+    private ListPreference mStatusBarBattery;
+    private ListPreference mBatteryPercentage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.statusbar);
+
+        mStatusBarBattery = (ListPreference) findPreference(STATUS_BAR_BATTERY_STYLE);
+        int batteryStyle = Settings.Secure.getInt(getActivity().getContentResolver(),
+                Settings.Secure.STATUS_BAR_BATTERY_STYLE, 0);
+        mStatusBarBattery.setValue(String.valueOf(batteryStyle));
+        mStatusBarBattery.setSummary(mStatusBarBattery.getEntry());
+        mStatusBarBattery.setOnPreferenceChangeListener(this);
+         mBatteryPercentage = (ListPreference) findPreference(BATTERY_PERCENT);
+        int showPercent = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.SHOW_BATTERY_PERCENT, 1);
+        mBatteryPercentage.setValue(Integer.toString(showPercent));
+        int valueIndex = mBatteryPercentage.findIndexOfValue(String.valueOf(showPercent));
+        mBatteryPercentage.setSummary(mBatteryPercentage.getEntries()[valueIndex]);
+        mBatteryPercentage.setOnPreferenceChangeListener(this);
+        boolean hideForcePercentage = batteryStyle == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT;
+        mBatteryPercentage.setEnabled(!hideForcePercentage);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+	if (preference == mStatusBarBattery) {
+            int battStyle = Integer.valueOf((String) newValue);
+            int index = mStatusBarBattery.findIndexOfValue((String) newValue);
+            Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.STATUS_BAR_BATTERY_STYLE, battStyle);
+            mStatusBarBattery.setSummary(mStatusBarBattery.getEntries()[index]);
+            boolean hideForcePercentage = battStyle == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT;
+            mBatteryPercentage.setEnabled(!hideForcePercentage);
+            return true;
+        } else  if (preference == mBatteryPercentage) {
+            int showPercent = Integer.valueOf((String) newValue);
+            int index = mBatteryPercentage.findIndexOfValue((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.SHOW_BATTERY_PERCENT, showPercent);
+            mBatteryPercentage.setSummary(mBatteryPercentage.getEntries()[index]);
+            return true;
+        }
+        return false;
     }
 
     @Override
